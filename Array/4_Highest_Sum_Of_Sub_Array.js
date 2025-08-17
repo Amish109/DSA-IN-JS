70cdad568f749d40441e6c365ff12ee4a1ea376b
Amish109 <tiwariamish786@gmail.com>
2025-08-17 16:44:33 +0530

    4_Highest_Sum_Of_Sub_Array.js

---
+++ Array/4_Highest_Sum_Of_Sub_Array.js
@@ -0,0 +1,32 @@
+// Given an array return the highest sum subarray
+// input : [-2,1,-3,4,-1,2,1,-5,4]
+// output 6
+/**
+ * 
+ */
+
+const calculateHighrdtConsecutiveSubArraySum = (data)=>{
+    // initial state
+    let max_sum = data[0];
+    let current_sum = data[0];
+
+    for(let i = 1;i<data.length; i++){
+        console.log('current sum',current_sum);
+        if(current_sum<0){
+            current_sum = data[i];
+        } else{
+            current_sum += data[i];
+        }
+        if(current_sum>max_sum){
+            max_sum = current_sum;
+        }
+    }
+
+    return max_sum;
+}
+let array = [-2,1,-3,4,-1,2,1,-5,4];
+// let array = [1,2,3,4,5,6,-50,30];
+// let array = [1,2,3,4,5,6,-2,30];
+// let array = [1,2,3,4,5,6];
+let result = calculateHighrdtConsecutiveSubArraySum(array);
+console.log('result',result);
