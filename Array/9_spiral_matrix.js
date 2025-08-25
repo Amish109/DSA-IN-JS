a72b7bec13a9e1ff93bf71f2b408085354b7a0e5
Amish109 <tiwariamish786@gmail.com>
2025-08-26 00:58:44 +0530

    spiral matix

--- Array/9_spiral_matrix.js
+++ Array/9_spiral_matrix.js
@@ -1,10 +1,38 @@
 // const input_data = [[1,2,3],[4,5,6],[7,8,9]];
+// const input_data =  [
+// 	[1, 2, 3, 4],
+// 	[5, 6,7 ,8] ,
+// 	[9, 10, 11,12],
+// 	[13,14,15,16]	
+// ];
 const input_data =  [
 	[1, 2, 3, 4],
 	[5, 6,7 ,8] ,
 	[9, 10, 11,12],
-	[13,14,15,16]	
+	[13,14,15,16]	,
+	[17,18,19,20]	,
+	[21,22,23,24]	,
 ];
+
+/**
+top =0
+right =3
+bottom =3
+left =0
+
+I-1) left:0 to right:3 [1,2,3,4]
+dir =1 and top =1
+
+top:1<=bottom:3 && left:0<=right:3 ? true
+
+I-2) iterate top to bottom 
+where col will be same (right:3) row top-bottom (1-3)
+
+
+
+
+
+ */
 // [
 // 	[1,2,3],
 // 	[4,5,6],
